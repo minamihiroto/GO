3cbf0cfa1fd4cb8bab6666e2a9d8ab6e929a1461
minamihiroto <kokowatamf@gmail.com>
2021-06-13 15:34:27 +0900

    変数について

--- app/main.go
+++ app/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	// var n int = 100
+	// var n = 200
+	n := 300 // 省略形
+	n = 400 // 変数の更新にはvarを意味する:をつけない
+	n -= 100
+	fmt.Println(n, n * 2)
 }
