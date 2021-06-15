64126df15f7ad1413bd0703e7ccd24d5b84309dc
minamihiroto <kokowatamf@gmail.com>
2021-06-15 21:42:00 +0900

    ポインタについて

--- app/main.go
+++ app/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(6))// sqrtは平方根を求める関数
+	name := "John"
+	fmt.Println(&name)//&をつけることでnameのポインタを参照できる
 }
