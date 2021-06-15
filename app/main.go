9b8c8b8343225a75df0b40647eef91d64d6cd4bb
minamihiroto <kokowatamf@gmail.com>
2021-06-15 21:55:19 +0900

    ポインタ型変数について

--- app/main.go
+++ app/main.go
@@ -5,6 +5,13 @@ import (
 )
 
 func main() {
-	name := "John"
-	fmt.Println(&name)//&をつけることでnameのポインタを参照できる
+	name := "MINAMI"
+	fmt.Println(&name)
+
+	namePtr := &name// var namePtr *string = &nameと同義で、ポインタ型は「*変数の型」で定義できる
+	fmt.Println(namePtr)//namePtrはnameのポインタを指している
+	fmt.Println(*namePtr)//*をつけることで"ポインタが示す変数の値"と言う意味になる
+
+	*namePtr = "HIROTO"//ポインタが示す変数の値に直接値を入れて更新することができる
+	fmt.Println(name)
 }
