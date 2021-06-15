e43d47b9005c0f356be4772aa84adbc18c0150db
minamihiroto <kokowatamf@gmail.com>
2021-06-15 10:50:59 +0900

    importをまとめる

--- app/main.go
+++ app/main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
-const Pi = 3.14
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	const World = "世界"//定数は、文字(character)、文字列(string)、boolean、数値(numeric)のみで使える
-	fmt.Println("Hello", World)
-	fmt.Println("Happy", Pi, "Day")
-
-	const Truth = true
-	fmt.Println("Go rules?", Truth)
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(6))// sqrtは平方根を求める関数
 }
