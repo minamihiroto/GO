b26bb9f21b476b976dd1f2809d54aeefe9cf7c06
minamihiroto <kokowatamf@gmail.com>
2021-06-16 19:50:40 +0900

    レシーバについて

--- app/main.go
+++ app/main.go
@@ -4,21 +4,16 @@ import (
 	"fmt"
 )
 
-func main() {
-	Person.Name = "minami"
-	Person.Age = 22
-	fmt.Println(Person)
-	fmt.Println(Person.Name)
-	fmt.Println(Person.Age)
-}
-
-var Person struct{// 変数に構造体を定義する
+type Person struct{// 型に構造体を定義する、これをレシーバという
 	Name string
 	Age int
 }
 
-// 構造体は非宣言文なので、何に（typeやvar）定義するのか決めないといけない
-// struct {
-//     Name string
-//     Age  int
-// }
+func main() {
+	human := Person{}//Person型レシーバをhumanに定義
+	human.Name = "minami"
+	human.Age = 22
+	fmt.Println(human)
+	fmt.Println(human.Name)
+	fmt.Println(human.Age)
+}
