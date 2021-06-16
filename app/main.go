19e034f8fa2c68a7f3137481009f2cb1bd1b769c
minamihiroto <kokowatamf@gmail.com>
2021-06-16 19:41:59 +0900

    構造体について

--- app/main.go
+++ app/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(6))// sqrtは平方根を求める関数
+	Person.Name = "minami"
+	Person.Age = 22
+	fmt.Println(Person)
+	fmt.Println(Person.Name)
+	fmt.Println(Person.Age)
 }
+
+var Person struct{// 変数に構造体を定義する
+	Name string
+	Age int
+}
+
+// 構造体は非宣言文なので、何に（typeやvar）定義するのか決めないといけない
+// struct {
+//     Name string
+//     Age  int
+// }
