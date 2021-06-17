22bf584153a174aaa5071bc646a253b1f90417f1
minamihiroto <kokowatamf@gmail.com>
2021-06-17 10:25:12 +0900

    メソッドについて

--- app/main.go
+++ app/main.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 )
 
-func main() {
-	a := 10
-	b := 10
-
-	calculate(a,&b)//この処理でmain関数のaは10,bは11になってる
-
-	fmt.Println("引数に整数を指定した場合：", a)
-	fmt.Println("引数にポインタを指定した場合：", b)
+type Result struct {//レシーバ
+	X, Y int
 }
 
-func calculate(a int, b *int) {
-	a += 1//あくまでここで定義されているaに更新が走ってるだけでmain関数のaとは関係ない
-	*b += 1//main関数のbのポインタを参照してるポインタ型変数を更新してる
+func (num Result) Sum() int {//これがメソッド、メソッドはレシーバを関数名定義前に書く、numにResultを代入
+	return num.X + num.Y
+}
+
+func main() {
+	I := Result{3, 4}//Iにレシーバを代入することで、メソッドを呼び出すことができる
+	fmt.Println(I.Sum())//足し算のメソッドを呼び出し
+
+	fmt.Println(I.X)
+	fmt.Println(I.Y)
+	fmt.Println(I.X + I.Y)//メソッドを呼び出した時と結果は同じ
 }
