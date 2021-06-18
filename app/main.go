bb4a9216b016dc74ad7a1f669af619b09b230776
minamihiroto <kokowatamf@gmail.com>
2021-06-18 20:08:28 +0900

    ポインタレシーバをメソッドに適用する方法について

--- app/main.go
+++ app/main.go
@@ -4,13 +4,26 @@ import (
 	"fmt"
 )
 
-type Result int//レシーバとしてtypeのみを定義、Resultというint型ができた（ただの別名）
+type Result struct{//構造体を持つレシーバ
+	X, Y int
+}
 
-func (num Result) Sum() int {//func後ろにレシーバを取るため、これはメソッド、numはResult型
-	return  int(num)
+func (num Result) Sum() int {//メソッド
+	return  num.X + num.Y
+}
+
+func (n *Result) Scale(f int) {//メソッドかつポインタ
+	n.Y = n.Y * f//2 * 3ということ
 }
 
 func main() {
-	I := Result(1)//Iにレシーバを代入
-	fmt.Println(I.Sum())
+	I := Result{1,2}//Iにレシーバを代入
+	fmt.Println(I.Y)//2
+	fmt.Println(I.X)//1
+	fmt.Println(I.Sum())//1 + 2
+
+	I.Scale(3)//Iの引き数（今回はY）を直接更新、3をscaleに渡してIにあるYを6に作り変えている
+	fmt.Println(I.Y)//6
+	fmt.Println(I.X)//1
+	fmt.Println(I.Sum())//1 + 6
 }
