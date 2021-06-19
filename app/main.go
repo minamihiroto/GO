a9101c250496d8379ccaeb297d92b494bde4bbc2
minamihiroto <kokowatamf@gmail.com>
2021-06-19 19:05:33 +0900

    nilについて

--- app/main.go
+++ app/main.go
@@ -2,19 +2,34 @@ package main
 
 import "fmt"
 
-type I interface {//インターフェースは中に関数が持てる、typeなので型
+func main() {
+	var i I
+	var t *T//*は"ポインタが示す変数の値"、ここで値はnil
+	i = t//I型のiにT型のtが入っただけ
+	P(i)//<nil>, *main.T
+	i.M()//<nil>
+
+	i = &T{"hello"}//T型のポインタをとってきてhelloをiに渡している、ポインタ先に値helloを指定
+	P(i)//&{hello}, *main.T
+	i.M()//hello
+}
+
+type I interface {
 	M()
 }
 
-type T struct {//ストラクトは中に変数が持てる、typeなので型
+type T struct {
 	S string
 }
 
-func (t T) M() {
+func (t *T) M() {//メソッドにポインタを渡している
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
 	fmt.Println(t.S)
 }
 
-func main() {
-	var i I = T{"hello"}//T型のhelloがiに入っていて、iはI型
-	i.M()//iはインターフェースのI型なので、M関数が使える
+func P(i I) {
+	fmt.Printf("%v, %T\n", i, i)//%vは値 == nil、%Tは型 == *main.T
 }
