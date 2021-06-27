9229b417e01e8ab89561be08430b6955668490d5
minamihiroto <kokowatamf@gmail.com>
2021-06-27 23:11:51 +0900

    パッケージと構成について１

--- app/main.go
+++ app/main.go
@@ -1,20 +1,12 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"gomodtest/animals"
+)
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-func (p Person) String() string {//String() string、これもインターフェース(Stringer)
-	//Stringerインターフェースはfmtパッケージに定義されている
-	//fmtパッケージ以外にも変数を文字列で出力するためのインタフェースがある
-	return fmt.Sprintf("%v (%v 歳)", p.Name, p.Age)
-}
-
-func main() {
-	a := Person{"山田太郎", 42}
-	z := Person{"田中エルフ", 9001}
-	fmt.Println(a, z)
+func main(){
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
 }
