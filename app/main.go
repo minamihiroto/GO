d197b99513a20e00d6940e73d01f8d338a8a10a5
minamihiroto <kokowatamf@gmail.com>
2021-06-30 00:18:25 +0900

    無名関数について

--- app/main.go
+++ app/main.go
@@ -4,18 +4,21 @@ import(
 	"fmt"
 )
 
-
-func div(a,b int)(int,int){
-	q := a+b
-	r := a-b
-	return q,r
+func returnFunc() func() {//func()が型
+	return func() {
+		fmt.Println("これは関数処理の結果を返しています")
+	}
 }
 
 func main(){
-	Q,R := div(2,1)	//割り振り代入
-	fmt.Println(Q,R)
+	//無名関数の基本
+	nanashi := func(x,y int)int{return x+y}//変数に関数リテラルを入れている
+	fmt.Println(nanashi(2,3))//変数()で呼び出せる
 
-	q1,_ := div(3,4)//戻り値の破棄
-	fmt.Println(q1)
+	//関数を直接呼び出すこともできる
+	fmt.Printf("%#v\n",func(x,y int)int{return x+y}(2,3))
 
+	//関数を返す関数
+	f := returnFunc()
+	f()
 }
