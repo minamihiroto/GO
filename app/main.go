6d0f0c30f82aedaf06d4a18fd9c7efc7923c5d83
minamihiroto <kokowatamf@gmail.com>
2021-06-30 00:21:19 +0900

    関数の別名について

--- app/main.go
+++ app/main.go
@@ -4,21 +4,13 @@ import(
 	"fmt"
 )
 
-func returnFunc() func() {//func()が型
-	return func() {
-		fmt.Println("これは関数処理の結果を返しています")
-	}
+func plus(x,y int) int {
+	return x+y
 }
 
+var plusAlias = plus//関数を代入、()をつけると実行してしまうのでつけない
+
 func main(){
-	//無名関数の基本
-	nanashi := func(x,y int)int{return x+y}//変数に関数リテラルを入れている
-	fmt.Println(nanashi(2,3))//変数()で呼び出せる
-
-	//関数を直接呼び出すこともできる
-	fmt.Printf("%#v\n",func(x,y int)int{return x+y}(2,3))
-
-	//関数を返す関数
-	f := returnFunc()
-	f()
+	fmt.Println(plus(10,5))
+	fmt.Println(plusAlias(10,5))//別名で呼び出せる
 }
