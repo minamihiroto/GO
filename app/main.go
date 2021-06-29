8cc5a8471ede60b89de6b095a1358e8db03d06d8
minamihiroto <kokowatamf@gmail.com>
2021-06-29 22:00:03 +0900

    配列について１

--- app/main.go
+++ app/main.go
@@ -5,9 +5,24 @@ import(
 )
 
 func main(){
-	v := `aaaa
-	bbbbbbb
-	aa`
-	
-	fmt.Print(v)
+	//初期値指定
+	v := [3]int{1,2,3}//変数 := [要素数]要素の型{要素}
+	n := [5]int{}
+	var a [5]int
+	z := [5]int{2,3,4}
+
+	fmt.Println(v,n,a,z)
+
+	//要素数の省略
+	v1 := [...]int{1,2,3}
+	v2 := [...]int{1,2,3,4,5}
+	v3 := [...]int{}
+
+	fmt.Println(v1,v2,v3)
+
+	//代入
+	v1[0] = 4
+	v2 = [...]int{5,4,3,2,1}
+
+	fmt.Println(v1,v2)
 }
