57ec196cc05b94e71da6c8a1ffd70528a12adfa7
minamihiroto <kokowatamf@gmail.com>
2021-07-01 20:39:36 +0900

    importのパッケージ名を上書きについて

--- app/main.go
+++ app/main.go
@@ -1,21 +1,14 @@
 package main
 
 import(
-	"fmt"
+	m "math"//mでパッケージ名を上書き
+	. "fmt"//.でパッケージ名を省略可
 )
 
 func main(){
-	const(//定数定義の値省略
-		X = 1
-		Y
-		Z
-		R = "あ"
-		L
-		S = iota//constブロックのなかで定数が定義されるたびに1ずつ増える
-		SS
-		SSS
-	)
-	fmt.Println(X,Y,Z)
-	fmt.Println(R,L)
-	fmt.Println(S,SS,SSS)
+	n := 1
+	Println(n)
+	//fmt.Println(n)//エラー
+	Println(m.Pi)
+	// Println(math.Pi)//エラー
 }
