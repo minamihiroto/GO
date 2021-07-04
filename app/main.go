8c3d01f6a3a3e8610fe5dadffc50adfce2737065
minamihiroto <kokowatamf@gmail.com>
2021-07-04 23:25:55 +0900

    if,forについて

--- app/main.go
+++ app/main.go
@@ -1,14 +1,46 @@
 package main
 
 import(
-	m "math"//mでパッケージ名を上書き
-	. "fmt"//.でパッケージ名を省略可
+	"fmt"
 )
 
 func main(){
-	n := 1
-	Println(n)
-	//fmt.Println(n)//エラー
-	Println(m.Pi)
-	// Println(math.Pi)//エラー
+	//if 条件式{処理}
+	//if 簡易文; 条件式{処理}
+	if x,y := 1,2; x < y{//x,y := 1,2;の部分が簡易文
+		fmt.Println(x,y)
+	}
+
+	//エラーが発生した時にも使える
+	//if _,err := 何らかの関数(); err != nil{
+	//	エラーが発生したときの処理
+	//}
+
+	// for 条件式{処理}
+	// for 初期化文; 条件式; 後処理文{処理}//古典的な書き方
+	
+	//break文は処理を中断させる (一番近くのfor,switch,selectを抜ける)
+	//continue文は残っている処理をスキップして次のループ処理にいく(一番近くのforで使用)
+
+	i := 0
+	// i := 10//breakで抜けない
+	for i<30{
+		fmt.Println(i)
+		i++
+
+		if i == 10{
+			fmt.Printf("%vになったから出るよ\n",i)	
+			break
+		}
+	}
+
+	for n:=0;n<10;n++{
+		if(n%2==1){
+			continue
+		}
+		fmt.Println(n)
+		n++
+	}
+
+
 }
