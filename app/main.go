064676e9e986947acbba8562bfa934cef738e75c
minamihiroto <kokowatamf@gmail.com>
2021-07-08 23:25:21 +0900

    ラベル付き文について

--- app/main.go
+++ app/main.go
@@ -5,11 +5,29 @@ import(
 )
 
 func main() {
-	//goto文で同じ関数内の任意の位置（ラベル）までスキップする
-	fmt.Println("A")
-	goto L//Lまで処理を飛ばす
-	fmt.Println("B")//実行されない
-	// a := a //エラーが起きる 👉 変数定義は飛ばせない
-	L://ラベル
-	fmt.Println("C")
+	//break ラベル
+  LOOP:
+	for{
+		for{
+			for{
+				fmt.Println("開始")
+				break LOOP// ラベル指定されてるfot文まで一気に抜ける
+			}
+			fmt.Println("ここは通らない")
+		}
+		fmt.Println("ここは通らない")
+	}
+	fmt.Println("完了")
+
+	//continue ラベル
+	L:
+	for i := 1;i <=3; i++{
+		for j:=1;j<=3; j++{
+			if j > 1{//1超えたらここに入る
+				continue L// ラベル指定されているfor文まで一気に戻る
+			}
+			fmt.Printf("%d*%d=%d\n",i,j,i*j)
+	  }
+	  fmt.Println("ここは通らない")
+  }
 }
