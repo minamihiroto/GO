2a4797ba212d256e8ce83e319a8242b974122ec0
minamihiroto <kokowatamf@gmail.com>
2021-07-08 23:51:57 +0900

    panicとrecoverについて

--- app/main.go
+++ app/main.go
@@ -5,12 +5,13 @@ import(
 )
 
 func main() {
-	runDefer()
-}
-
-func runDefer(){
-	//deferで定義すると関数の終了時に実行される
-	defer fmt.Println("deferを実行2回目")
-	defer fmt.Println("deferを実行")//最後に書かれたdeferがdeferの中で一番初めに実行される
-	fmt.Println("done")
+	//panicは関数内の処理を中断させてdefer文のみを実行する
+	//recoverはinterface{}型を返し、nil以外だった場合はpanicによる中断を回復させる
+	defer func(){
+		if x := recover(); x != nil{
+			fmt.Println(x)
+		}
+	}()
+	panic("panic!")
+	fmt.Println("hello")//ここは実行されない
 }
