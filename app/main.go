b58ff1e8c907fa29e802aed1d00761d293d8c0c8
minamihiroto <kokowatamf@gmail.com>
2021-07-08 23:51:45 +0900

    deferについて

--- app/main.go
+++ app/main.go
@@ -5,29 +5,12 @@ import(
 )
 
 func main() {
-	//break ラベル
-  LOOP:
-	for{
-		for{
-			for{
-				fmt.Println("開始")
-				break LOOP// ラベル指定されてるfot文まで一気に抜ける
-			}
-			fmt.Println("ここは通らない")
-		}
-		fmt.Println("ここは通らない")
-	}
-	fmt.Println("完了")
+	runDefer()
+}
 
-	//continue ラベル
-	L:
-	for i := 1;i <=3; i++{
-		for j:=1;j<=3; j++{
-			if j > 1{//1超えたらここに入る
-				continue L// ラベル指定されているfor文まで一気に戻る
-			}
-			fmt.Printf("%d*%d=%d\n",i,j,i*j)
-	  }
-	  fmt.Println("ここは通らない")
-  }
+func runDefer(){
+	//deferで定義すると関数の終了時に実行される
+	defer fmt.Println("deferを実行2回目")
+	defer fmt.Println("deferを実行")//最後に書かれたdeferがdeferの中で一番初めに実行される
+	fmt.Println("done")
 }
