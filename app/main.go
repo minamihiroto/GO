eb62da20dad51e135707503be4621d4ce22e4e80
minamihiroto <kokowatamf@gmail.com>
2021-07-09 20:58:30 +0900

    go文について

--- app/main.go
+++ app/main.go
@@ -4,14 +4,19 @@ import(
 	"fmt"
 )
 
+func sub(){
+	for{//無限ループ
+		fmt.Println(1)
+  }
+}
+
+func sub2(){
+	for{//無限ループ
+		fmt.Println(2)
+  }
+}
+
 func main() {
-	//panicは関数内の処理を中断させてdefer文のみを実行する
-	//recoverはinterface{}型を返し、nil以外だった場合はpanicによる中断を回復させる
-	defer func(){
-		if x := recover(); x != nil{
-			fmt.Println(x)
-		}
-	}()
-	panic("panic!")
-	fmt.Println("hello")//ここは実行されない
+	go sub() //goで関数を実行すると、並列処理が行われる
+	sub2()
 }
