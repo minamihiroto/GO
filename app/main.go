0048ba391f4964e433324d8b9db066dab1894a61
minamihiroto <kokowatamf@gmail.com>
2021-07-11 18:30:24 +0900

    スライスの基礎と配列との比較について

--- app/main.go
+++ app/main.go
@@ -4,14 +4,24 @@ import(
 	"fmt"
 )
 
-func init(){//main関数より先に実行される
-	fmt.Println("init()")
-}
-
-func init(){//同じファイルに何回でも定義できる 👉 普通はしない
-	fmt.Println("aaa")
-}
-
 func main() {
-	fmt.Println("main()")
+	//配列の作成
+	var a [10]int
+	fmt.Println(a)
+	//配列への代入
+	a[0] = 1
+	fmt.Println(a)
+	fmt.Println(a[0])
+	//配列の要素数を調べる
+	fmt.Println(len(a))
+
+	//スライスの作成
+	s:=make([]int,10)
+	fmt.Println(s)
+	//スライスへの代入 👉 配列と同じ
+	s[0] = 1
+	fmt.Println(s)
+	fmt.Println(s[0])
+	//スライスの要素数を調べる 👉 配列と同じ
+	fmt.Println(len(s))
 }
